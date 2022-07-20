fdc16887b21a2059953bda03f52fa96ca4c082ce
Deyan Petrov <deyanpetrov89@yahoo.com>
2022-07-20 09:51:12 +0200

    clean up main page from unused code

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,16 +2,9 @@ import Head from 'next/head';
 import { useState } from 'react';
 import { GET_ALL_PRODUCTS } from '../graphql/Queries';
 import { useQuery } from '@apollo/client';
-import InputForm from '../components/InputForm';
 
 export default function Home() {
   const { loading, error, data } = useQuery(GET_ALL_PRODUCTS);
-  // const [camera, setCamera] = useState(false);
-  // const [result, setResult] = useState(null);
-
-  // const onDetected = (result) => {
-  //   setResult(result);
-  // };
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
@@ -28,16 +21,6 @@ export default function Home() {
       </Head>
       <h1 className='text-5xl font-bold text-center'>Hello</h1>
       <div className='flex flex-col'>
-        {/* <BarcodeScannerNoSSR /> */}
-        {/* <div>
-            <p>{result ? result : 'Scanning...'}</p>
-            <button onClick={() => setCamera(!camera)}>
-              {camera ? 'Stop' : 'Start'}
-            </button>
-            <div className='container'>
-              {camera && <BarcodeScanner onDetected={onDetected} />}
-            </div>
-          </div> */}
         {data.getAllProducts.map((product, id) => (
           <div key={id}>
             <h1>Name: {product.name}</h1>
